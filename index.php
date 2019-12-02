8fab60eeae965b33bc5d6247169b41ccfbe1adc1
Justice <justicemarkwei@gmail.com>
2019-12-02 15:36:28 +0000

    first commit from localhost

---
+++ index.php
@@ -0,0 +1,160 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Cheina Care Givers</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" type="image/png" href="img/favicon.png" />
+
+    <!-- Styles -->
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.10/css/mdb.min.css" rel="stylesheet"> -->
+
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
+
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/main.css">
+
+    <!-- Javascript dependencies -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+</head>
+
+<body>
+
+    <!--Navbar -->
+    <nav class="navbar  navbar-expand-lg fixed-top bg-light-grey">
+        <div class="container">
+            <a class="navbar-brand link-unstyled" href="./">
+                <img src="img/logo.png" width="80" alt="Cheina Care Givers"> Cheina Care Givers
+            </a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav my-2 my-lg-0 ml-auto">
+                    <li class="nav-item nav-item-padding active">
+                        <a id="home" class="nav-link text-dark float-right nav-active" href="./">HOME <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item nav-item-padding active">
+                        <a class="nav-link text-dark float-right" href="about">ABOUT<span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item nav-item-padding active">
+                        <a class="nav-link text-dark float-right" href="our-people">SERVICES <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item nav-item-padding active">
+                        <a class="nav-link text-dark float-right" href="prospects">SHOWCASE <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item nav-item-padding active">
+                        <a class="nav-link text-dark float-right" href="work-with-us">BLOG <span class="sr-only">(current)</span></a>
+                    </li>
+                    <li class="nav-item nav-item-padding active">
+                        <a class="nav-link text-dark float-right" href="blog/">CONTACT <span class="sr-only">(current)</span></a>
+                    </li>
+
+                </ul>
+                <ul class="navbar-nav my-2 my-lg-0 ml-auto">
+                    <li class="nav-item nav-item-padding active">
+                        <button class="btn btn-primary btn-lg">SIGN UP</button>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <!--/.Navbar -->
+
+    <header class="py-5 header-banner d-md-flex">
+        <div class="container my-md-auto">
+            <div class="row">
+                <div class="col-md-5">
+                    <div class="">
+                        <h1 class="font-bold">
+                            Find care home jobs the easy way
+                        </h1>
+                        <h5>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad ab quaerat cum odio reprehenderit aut, a ea at ex molestias tempore esse, provident possimus animi labore. Esse obcaecati porro saepe.
+                        </h5>
+                        <a class="btn btn-primary btn-lg" href="#">READ MORE</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- Sign up with chiena -->
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                    <h2 class="font-bold">Sign up with Chiena Care Givers in Under a minute</h2>
+                    <p class="h5">You'll need:</p>
+
+                    <ul>
+                        <li class="mt-4">
+                            <h5>At least one year of work experience</h5>
+                        </li>
+                        <li class="mt-4">
+                            <h5>Up-to-date training certificates</h5>
+                        </li>
+                        <li class="mt-4">
+                            <h5>
+                                Two references
+                            </h5>
+                        </li>
+                        <li class="mt-4">
+                            <h5>
+                                Passport/Biometric ID Card (to confirm your identity)
+                            </h5>
+                        </li>
+                    </ul>
+
+                    <h5 class="mt-4">
+                        See a <a class="h5" href="#">detailed checklist</a> of everything you need to start
+                        booking shifts on Cheina Care Givers.
+                    </h5>
+                </div>
+                <div class="col-md-6">
+                    <div class="card w-75 mx-auto box-shadow">
+                        <div class="card-body">
+                            <form>
+                                <div class="form-group">
+                                    <label for="name">Name</label>
+                                    <input type="text" class="form-control" id="name" aria-describedby="name" placeholder="Enter name">
+                                </div>
+                                <div class="form-group">
+                                    <label for="email">Password</label>
+                                    <input type="email" class="form-control" id="email" placeholder="Email">
+                                </div>
+                                <div class="form-group">
+                                    <label for="password">Password</label>
+                                    <input type="password" class="form-control" id="password" placeholder="Password">
+                                </div>
+                                <div class="form-group form-check">
+                                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                    <label class="form-check-label" for="exampleCheck1">
+                                        I agree to Cheina Care Givers's <a href="#"> terms & conditions</a> and <a href="#">privacy policy</a>
+                                    </label>
+                                </div>
+                                <button type="submit" class="btn btn-primary btn-lg float-right">SIGN UP</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+</body>
+
+</html>
