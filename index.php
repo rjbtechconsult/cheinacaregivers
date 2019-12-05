af038b445162abf17f0dcc9c5ea68ad26ab74619
Justice <justicemarkwei@gmail.com>
2019-12-05 12:11:10 +0000

    homepage

--- index.php
+++ index.php
@@ -12,13 +12,10 @@
 
     <!-- Styles -->
     <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
-
+    <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"> -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.8.10/css/mdb.min.css" rel="stylesheet"> -->
-
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
-
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="stylesheet" href="fontawesome-free-5.11.2-web/css/all.css">
     <link rel="stylesheet" href="css/main.css">
 
     <!-- Javascript dependencies -->
@@ -26,6 +23,7 @@
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <script src="fontawesome-free-5.11.2-web/js/all.js"></script>
 
 </head>
 
@@ -38,32 +36,33 @@
                 <img src="img/logo.png" width="80" alt="Cheina Care Givers"> Cheina Care Givers
             </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                <!-- <span class="navbar-toggler-icon"></span> -->
+                <i class="fa fa-bars fa-lg"></i>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav my-2 my-lg-0 ml-auto">
+                <ul class="navbar-nav my-2 my-lg-0 ml-auto text-center">
                     <li class="nav-item nav-item-padding active">
-                        <a id="home" class="nav-link text-dark float-right nav-active" href="./">HOME <span class="sr-only">(current)</span></a>
+                        <a id="home" class="nav-link text-dark text-center nav-active" href="./">HOME <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item nav-item-padding active">
-                        <a class="nav-link text-dark float-right" href="about">ABOUT<span class="sr-only">(current)</span></a>
+                        <a class="nav-link text-dark text-center" href="#">ABOUT<span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item nav-item-padding active">
-                        <a class="nav-link text-dark float-right" href="our-people">SERVICES <span class="sr-only">(current)</span></a>
+                        <a class="nav-link text-dark text-center" href="#">SERVICES <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item nav-item-padding active">
-                        <a class="nav-link text-dark float-right" href="prospects">SHOWCASE <span class="sr-only">(current)</span></a>
+                        <a class="nav-link text-dark text-center" href="#">SHOWCASE <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item nav-item-padding active">
-                        <a class="nav-link text-dark float-right" href="work-with-us">BLOG <span class="sr-only">(current)</span></a>
+                        <a class="nav-link text-dark text-center" href="#">BLOG <span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item nav-item-padding active">
-                        <a class="nav-link text-dark float-right" href="blog/">CONTACT <span class="sr-only">(current)</span></a>
+                        <a class="nav-link text-dark   text-center" href="#">CONTACT <span class="sr-only">(current)</span></a>
                     </li>
 
                 </ul>
                 <ul class="navbar-nav my-2 my-lg-0 ml-auto">
-                    <li class="nav-item nav-item-padding active">
+                    <li class="nav-item nav-item-padding active text-center">
                         <button class="btn btn-primary btn-lg">SIGN UP</button>
                     </li>
                 </ul>
@@ -72,11 +71,12 @@
     </nav>
     <!--/.Navbar -->
 
-    <header class="py-5 header-banner d-md-flex">
+    <!-- Banner section -->
+    <header class="py-5 header-banner">
         <div class="container my-md-auto">
-            <div class="row">
-                <div class="col-md-5">
-                    <div class="">
+            <div class="row d-flex">
+                <div class="col-md-5 header-col">
+                    <div class="text-center text-md-left">
                         <h1 class="font-bold">
                             Find care home jobs the easy way
                         </h1>
@@ -94,10 +94,11 @@
     <section>
         <div class="container">
             <div class="row">
-                <div class="col-md-6">
-                    <h2 class="font-bold">Sign up with Chiena Care Givers in Under a minute</h2>
+                <div class="col-md-6 ">
+                    <div class="text-center text-md-left">
+                        <h1 class="font-bold">Sign up with Chiena Care Givers in Under a minute</h1>
+                    </div>
                     <p class="h5">You'll need:</p>
-
                     <ul>
                         <li class="mt-4">
                             <h5>At least one year of work experience</h5>
@@ -144,7 +145,7 @@
                                         I agree to Cheina Care Givers's <a href="#"> terms & conditions</a> and <a href="#">privacy policy</a>
                                     </label>
                                 </div>
-                                <button type="submit" class="btn btn-primary btn-lg float-right">SIGN UP</button>
+                                <button type="submit" class="btn btn-primary btn-lg float-right ">SIGN UP</button>
                             </form>
                         </div>
                     </div>
@@ -153,7 +154,125 @@
         </div>
     </section>
 
+    <!-- Connects nurses -->
+    <section>
+        <div class="container">
+            <div class="row flex-column-reverse flex-md-row">
+                <div class="col-md-6">
+                    <img class="w-100" src="img/nurses.png" alt="Connects nurses and career">
+                </div>
 
+                <div class="col-md-6 d-flex">
+                    <div class="my-auto text-md-left text-center">
+                        <h1 class="font-bold">Cheina Care Givers connects nurses and carers to care homes across Ghana </h1>
+                        <h5>
+                            We’re simplifying booking temporary shifts, making it easier than ever to be an independent nurse or carer.
+                        </h5>
+                        <button class="btn btn-primary btn-lg">SIGN UP</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!-- Process  -->
+    <section>
+        <div class="container">
+            <div class="row">
+
+                <div class="col-md-4">
+                    <div class="card w-100" style="border:none;">
+                        <img class="card-img-top w-75 mx-auto" src="img/hold_calendar.png" alt="Homes post their shifts ">
+                        <div class="card-body text-center">
+                            <h5 class="card-title">Homes post their shifts to Cheina Care Givers daily</h5>
+                            <p class="card-text">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risusas facilisis.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-md-4">
+                    <div class="card w-100" style="border:none;">
+                        <img class="card-img-top w-75 mx-auto" src="img/writing.png" alt="pick the shifts you want">
+                        <div class="card-body text-center">
+                            <h5 class="card-title">Pick the shifts you want to work in one click</h5>
+                            <p class="card-text">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risusas facilisis.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+
+                <div class="col-md-4">
+                    <div class="card w-100" style="border:none;">
+                        <img class="card-img-top w-75 mx-auto" src="img/clock.png" alt="Submit your timesheet online">
+                        <div class="card-body text-center">
+                            <h5 class="card-title">Submit your timesheet online, anytime, anywhere</h5>
+                            <p class="card-text">
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risusas facilisis.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </section>
+
+    <!-- Footer -->
+    <footer class="mt-5 pt-5 pb-5 footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-5 col-xs-12 about-company links text-center text-md-left">
+                    <h2>CHIENA CARE GIVERS</h2>
+                    <ul class="m-0 p-0">
+                        <li><a href="#">Our Story</a></li>
+                        <li><a href="#">Open Positions</a></li>
+                        <li><a href="#">Our Programmes</a></li>
+                        <li><a href="#">Empower Us</a></li>
+                        <li><a href="#">How Cheina Care Givers Works</a></li>
+                        <li><a href="#">Blog</a></li>
+                    </ul>
+                    <!-- <p class="pr-5 text-white-50">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam ac ante mollis quam tristique convallis </p>
+                    <p><a href="#"><i class="fa fa-facebook-square mr-1"></i></a><a href="#"><i class="fa fa-linkedin-square"></i></a></p> -->
+                </div>
+                <div class="col-lg-3 col-xs-12 links text-center text-md-left">
+                    <h4 class="mt-lg-0 mt-sm-3">MORE</h4>
+                    <ul class="m-0 p-0">
+                        <li><a href="#">Published on</a></li>
+                        <li><a href="#">News & Press</a></li>
+                        <li><a href="#">FAQ</a></li>
+                    </ul>
+                </div>
+                <div class="col-lg-4 col-xs-12 location links text-center text-md-left">
+                    <h4 class="mt-lg-0 mt-sm-4">CONNECT</h4>
+                    <ul class="m-0 p-0">
+                        <li><a href="#"><i class="fab fa-facebook text-white mr-2"></i>Facebook</a></li>
+                        <li><a href="#"><i class="fab fa-twitter text-white mr-2"></i>Twitter</a></li>
+                        <li><a href="#"><i class="fab fa-linkedin text-white mr-2"></i>LinkedIn</a></li>
+                        <li><a href="#"><i class="fab fa-instagram text-white mr-2"></i>Instagram</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div class="row mt-5">
+                <div class="col copyright text-center text-md-left">
+                    <p>
+                        <small class="text-white-50">© <span id="current_year"></span>. All Rights Reserved.</small>
+                        <small class="text-white-50">Powered by <a href="https://teamalfy.com" target="_blank">TeamAlfy</a> </small>
+                    </p>
+
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- ./Footer -->
+
+    <script>
+        var currentYear = (new Date).getFullYear();
+        $("#current_year").text((new Date).getFullYear());
+    </script>
 
 </body>
 
